3cbe97475fbf52e911bf3979f450c9ec05c6d8ec
Jon Johnston <johnst.jonr@gmail.com>
2021-01-11 12:16:42 -0600

    Create LinkedList class with basic functionality

--- data_structures.py
+++ data_structures.py
@@ -12,3 +12,39 @@ class Node:
     def get_next_node(self):
         return self.next_node
 
+class LinkedList:
+    def __init__(self, head_node = None):
+        self.head_node = head_node
+    
+    def __repr__(self):
+        list_string = ''
+        current_node = self.head_node
+        while current_node:
+            list_string += str(current_node.get_value()) + '\n'
+            current_node = current_node.get_next_node()
+        return list_string
+
+    def __len__(self):
+        current_node = self.head_node
+        count = 0
+        while current_node:
+            count += 1
+            current_node = current_node.get_next_node()
+        return count
+    
+    def set_head_node(self, head_node):
+        head_node.set_next_node(self.head_node)
+        self.head_node = head_node
+    
+    def add_node(self, new_node):
+        current_node = self.head_node
+        while current_node.get_next_node():
+            current_node = current_node.get_next_node()
+        current_node.set_next_node(new_node)
+    
+
+li = LinkedList(Node('yo'))
+li.add_node(Node(47))
+li.add_node(Node(['bengals', 'dolphins', 'saints']))
+
+print(len(li))
